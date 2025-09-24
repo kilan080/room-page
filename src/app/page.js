a8eba44d11a381ffda062591ccb0bdd76a15ad0f
kilan080 <kilanioluwapheranmie@gmail.com>
2025-09-24 10:19:15 +0100

    correct

--- src/app/page.js
+++ src/app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
   };
 
   return (
-    <main className="grid h-screen grid-cols-1 md:grid-cols-2">
+    <main className="grid grid-cols-1 md:grid-cols-2">
       {/* Row 1: Navbar + Discover */}
       <div className="col-span-1 md:col-span-2 grid grid-cols-1 md:grid-cols-2">
         <Navbar backgroundImage={images[imageIndex]} togglePrevImage={togglePrevImage} toggleNextImage={toggleNextImage} />
